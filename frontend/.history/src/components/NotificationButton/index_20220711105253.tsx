911104b58d2d2be2d02cc942c54eec832b88d08a
mizaeldouglas <mizaeldouglas2@gmail.com>
2022-07-11 11:00:01 -0300

    First component

---
+++ frontend/.history/src/components/NotificationButton/index_20220711105253.tsx
@@ -0,0 +1,9 @@
+function NotificationButton() {
+  return (
+    <div className="dsmeta-red-btn">
+      <img src="../../assets/img/notification-icon.svg" alt="Notificar" />
+    </div>
+  );
+}
+
+export default NotificationButton;
